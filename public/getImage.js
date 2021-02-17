93758fd4180e2e57bc398f2f12853a9dab9abc8a
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-02-17 17:39:23 +0900

    change Setup for Momentum2.0

--- public/getImage.js
+++ public/getImage.js
@@ -1,14 +1,11 @@
 const body = document.querySelector('body');
-const numOfImg = 4;
+const imgName = 4;
 
 const applyImg = (num) => {
   const backgroundImg = document.createElement('img');
-  backgroundImg.src = `image/${num + 1}.jpg`;
+  backgroundImg.src = `./image/${num}.jpg`;
   backgroundImg.classList.add('bgImage');
   body.append(backgroundImg);
 };
-function init() {
-  const num = Math.floor(Math.random() * numOfImg);
-  applyImg(num);
-}
-init();
+
+applyImg(imgName);
--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const app = express();
-const hbs = require('hbs');
+const hbs = require('express-handlebars');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-const viewsPath = path.join(__dirname, './hbs/views');
-const partialsPath = path.join(__dirname, './hbs/partials');
+const layoutPath = path.join(__dirname, './views/layouts');
+const partialsPath = path.join(__dirname, './views/partials');
 const publicPath = path.join(__dirname, './public');
 
 app.set('view engine', 'hbs');
-app.set('views', viewsPath);
-hbs.registerPartials(partialsPath);
+app.engine(
+  'hbs',
+  hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: layoutPath,
+    partialsDir: partialsPath,
+  })
+);
 
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    js: ['getTime'],
+  });
 });
 
 app.listen(PORT, () => {
