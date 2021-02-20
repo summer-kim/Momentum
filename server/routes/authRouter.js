baf96bc88669660402b00c72a4638252d4b6c794
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-02-20 18:59:28 +0900

    make Register page and Error msg for a while

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -15,13 +15,39 @@ router.post('/login', async (req, res) => {
     auth.onAuthStateChanged((user) => {
       if (user) {
         return res.redirect('/');
-      } else {
-        return res.redirect('/login');
       }
     });
   } catch (err) {
     res.status(400).json({ msg: err.message });
+    return res.redirect('/login');
   }
 });
 
+router.get('/register', (req, res) => {
+  res.render('register', {
+    style: 'login',
+  });
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password, userName } = req.body;
+    await auth.createUserWithEmailAndPassword(email, password);
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        await user.updateProfile({
+          displayName: userName,
+        });
+        return res.redirect('/login');
+      } else {
+        return res.redirect('/register');
+      }
+    });
+  } catch (err) {
+    res.status(400).render('register', {
+      msg: err.message,
+      style: 'login',
+    });
+  }
+});
 module.exports = { routes: router };
