384869504ef82e5d6144842250f0592df98b001f
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-02-23 17:17:42 +0900

    change method of data being stored

--- server/routes/todoRouter.js
+++ server/routes/todoRouter.js
@@ -7,7 +7,10 @@ router.get('/data/folder/:folderName', async (req, res) => {
     const user = auth.currentUser;
     const folderName = req.params.folderName;
 
-    await db.collection(user.displayName).doc(folderName).set({});
+    await db
+      .collection(user.displayName)
+      .doc('folders')
+      .set({ [folderName]: [] }, { merge: true });
     return res.json({ msg: 'set Folder Successfully' });
   } catch (error) {
     res.status(400).send(error.message);
