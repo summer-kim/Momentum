fe342b01af08476dd7a91aeb7d81953a57c89a36
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-02-25 21:21:33 +0900

    make todo-title and addTodo function

--- public/js/displayTodos.js
+++ public/js/displayTodos.js
@@ -1,12 +1,18 @@
 import { getFolder } from './getTodo.js';
 
+const todoForm = document.querySelector('.todoForm');
+const todoTitle = document.querySelector('.todo-title');
+
 export const onClickGetTodo = async (e) => {
   const selectedBefore = document.querySelector('.selected');
   if (selectedBefore) {
     selectedBefore.classList.remove('selected');
   }
   e.target.classList.add('selected');
+  todoForm.classList.remove('dp-none');
   const Name = e.target.innerText;
+
+  todoTitle.innerText = Name;
 
   const isLink = e.target.classList.contains('link');
   const todos = await getFolder(Name, isLink);
@@ -16,7 +22,9 @@ export const onClickGetTodo = async (e) => {
   while (todoList.firstChild) {
     todoList.firstChild.remove();
   }
-  todos.forEach((todo) => displayTodo(todo, todoList));
+  if (todos.length > 0) {
+    todos.forEach((todo) => displayTodo(todo, todoList));
+  }
 };
 
 export const displayTodo = (todo, todoList) => {
@@ -31,6 +39,14 @@ export const displayTodo = (todo, todoList) => {
   deletebtn.addEventListener('click', deleteTodo);
 };
 
-const deleteTodo = (e) => {
+const deleteTodo = async (e) => {
   console.log(e.target);
 };
+
+export const addTodo = (e) => {
+  e.preventDefault();
+  const value = todoForm.firstElementChild.value;
+  console.log(value);
+};
+
+todoForm.addEventListener('submit', addTodo);
