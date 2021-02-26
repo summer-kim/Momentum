1793da6efd69a882a2cba6326ce70f5bdeba110a
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-02-26 16:04:52 +0900

    make delete Folder/Link button

--- public/js/getWeather.js
+++ public/js/getWeather.js
@@ -2,8 +2,7 @@ export const setWeather = async (city) => {
   const url = '/data/weather/' + city;
 
   try {
-    const res = await fetch(url);
-    const data = await res.json();
+    await fetch(url);
   } catch (err) {
     console.log(err);
   }
