26ea119d2a076ec72592462138bcf5223b170730
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-09 17:55:29 +0900

    reset

--- server/db.js
+++ server/db.js
@@ -1,6 +1,26 @@
 const firebase = require('firebase');
-const config = require('config');
-const firebaseConfig = config.get('firebaseConfig');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const {
+  APIKEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: APIKEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
