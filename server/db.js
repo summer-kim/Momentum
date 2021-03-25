eb4700f4729056a155f440eafb6a986bf80aeafd
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-25 17:50:35 +0900

    make db to use process.env instead of json file

--- server/db.js
+++ server/db.js
@@ -3,8 +3,31 @@ dotenv.config();
 
 //middleware
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+const {
+  TYPE,
+  PROJECT_ID,
+  PRIVATE_KEY_ID,
+  PRIVATE_KEY,
+  CLIENT_EMAIL,
+  CLIENT_ID,
+  AUTH_URI,
+  TOKEN_URI,
+  AUTH_PROVIDER_X509_CERT_URL,
+  CLIENT_X509_CERT_URL,
+} = process.env;
 
+const serviceAccount = {
+  type: TYPE,
+  project_id: PROJECT_ID,
+  private_key_id: PRIVATE_KEY_ID,
+  private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: CLIENT_EMAIL,
+  client_id: CLIENT_ID,
+  auth_uri: AUTH_URI,
+  token_uri: TOKEN_URI,
+  auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: CLIENT_X509_CERT_URL,
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
