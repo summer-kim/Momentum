9172cf1fc90ddecaf9f7a6fb8f22463703bff9a4
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-26 17:21:15 +0900

    erase console.log

--- server/middleware.js
+++ server/middleware.js
@@ -3,9 +3,7 @@ const { admin } = require('./db');
 
 const verifyUser = async (req, res, next) => {
   const sessionCookie = req.cookies.session || '';
-  console.log('verify');
   if (!sessionCookie) {
-    console.log('redirect');
     return res.redirect('/login');
   }
   try {
@@ -13,14 +11,11 @@ const verifyUser = async (req, res, next) => {
       .auth()
       .verifySessionCookie(sessionCookie, true)
       .then((decodedClaims) => {
-        console.log('decodedClaims :' + decodedClaims.uid);
-
         req.uid = decodedClaims.uid;
         req.userName = decodedClaims.name;
         next();
       });
   } catch (err) {
-    console.log('verify err :' + err.message);
     res.status(400).render('login', {
       err: err.message,
       style: 'login',
