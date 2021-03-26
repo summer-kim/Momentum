646efbee9731ce7d5c1992237ba18ba351c1d708
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-26 16:01:19 +0900

    redirect to login page (Heroku error)

--- server/middleware.js
+++ server/middleware.js
@@ -4,16 +4,23 @@ const { admin } = require('./db');
 const verifyUser = async (req, res, next) => {
   const sessionCookie = req.cookies.session || '';
   console.log('verify');
+  if (!sessionCookie) {
+    console.log('redirect');
+    return res.redirect('/');
+  }
   try {
     admin
       .auth()
       .verifySessionCookie(sessionCookie, true)
       .then((decodedClaims) => {
+        console.log('decodedClaims :' + decodedClaims.uid);
+
         req.uid = decodedClaims.uid;
         req.userName = decodedClaims.name;
         next();
       });
   } catch (err) {
+    console.log('verify err :' + err.message);
     res.status(400).render('login', {
       err: err.message,
       style: 'login',
