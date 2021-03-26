7b6420e032422274c341355186d5b0d1e7e48ff7
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-26 15:33:27 +0900

    revise server.js for Heroku error

--- server.js
+++ server.js
@@ -6,21 +6,10 @@ const hbs = require('express-handlebars');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(publicPath));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(csrfMiddleware);
-
 const csrfMiddleware = csrf({ cookie: true });
-
 const layoutsDir = path.join(__dirname, './views/layouts');
 const partialsDir = path.join(__dirname, './views/partials');
 const publicPath = path.join(__dirname, './public');
-
-const authRouter = require('./server/routes/authRouter');
-const weatherRouter = require('./server/routes/weatherRouter');
-const todoRouter = require('./server/routes/todoRouter');
 
 app.set('view engine', 'hbs');
 app.engine(
@@ -35,6 +24,15 @@ app.engine(
 if (process.env.NODE_ENV === 'production') {
   app.get('view cache');
 }
+app.use(express.static(publicPath));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+const authRouter = require('./server/routes/authRouter');
+const weatherRouter = require('./server/routes/weatherRouter');
+const todoRouter = require('./server/routes/todoRouter');
 
 const verifyUser = require('./server/middleware');
 app.use('/weather', verifyUser, weatherRouter.routes);
